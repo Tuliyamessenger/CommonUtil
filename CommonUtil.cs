9a2c7e697a25a941eadb1fada45438b0c66af9ba
Yi Fang <50725927+Tuliyamessenger@users.noreply.github.com>
2023-06-20 01:52:31 +0800

    Add files via upload

--- CommonUtil.cs
+++ CommonUtil.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,5 +12,47 @@ public static class CommonUtil
         screenPos /= canvasRect.localScale;
         screenPos -= canvasRect.sizeDelta * anchor;
         return screenPos;
+    }
+
+    /// <summary>
+    /// 为动作添加监听，忽略相同监听。
+    /// </summary>
+    static public void AddListener(ref Action action, Action listener) {
+        if(!ActionHasListener(action, listener)) {
+            action += listener;
+        }
+    }
+
+    /// <summary>
+    /// 为动作添加监听，忽略相同监听。
+    /// </summary>
+    static public void AddListener<T>(ref Action<T> action, Action<T> listener) {
+        if (!ActionHasListener(action, listener)) {
+            action += listener;
+        }
+    }
+
+    /// <summary>
+    /// 为动作添加监听，忽略相同监听。
+    /// </summary>
+    static public void AddListener<T1, T2>(ref Action<T1, T2> action, Action<T1, T2> listener) {
+        if (!ActionHasListener(action, listener)) {
+            action += listener;
+        }
+    }
+
+    /// <summary>
+    /// 判断该动作是否存在指定监听。
+    /// </summary>
+    static public bool ActionHasListener(Delegate action, Delegate listener){
+        if (action != null) {
+            Delegate[] delegates = action.GetInvocationList();
+            for (int i = 0; i < delegates.Length; i += 1) {
+                if (delegates[i].Equals(listener)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
